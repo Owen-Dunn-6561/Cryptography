96fdc7b132e1338d1753173191685a0654b28885
Owen Dunn <owenrdunn03@gmail.com>
2025-08-25 17:27:18 -0400

    BaseChanger.java

--- BaseChanger.java
+++ BaseChanger.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-
+// Changes from Hexidecimal to Base 64
 public class BaseChanger {
 
         public static void main(String[] args) {
@@ -62,3 +62,4 @@ public class BaseChanger {
 
         }
 }
+
